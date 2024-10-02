4f45b789c0c910db1e3c2607945ad6890e478972
Antonio Costas <91885884+antonioc8021@users.noreply.github.com>
2024-10-02 17:51:03 +0200

    First steps

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// se usa el % en las variables que almacenan contenido del html por convenio no tiene otra finalidad no es como en php que es obligatorio
+const $time = document.querySelector('time');
+const $p = document.querySelector('p');
+const $input = document.querySelector('input');
+
+// todo: seguir con el proyecto
